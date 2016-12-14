caf2f11f868e86d33b0da75357bf5c47b5c1e16c
Jimmy Song <jimmy.song@aliyun.com>
2016-12-15 01:51:09 +0800

    add editor config & format text by editorconfig\eslint

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 const electron = require('electron');
 
-const { app, BrowserWindow } = electron;
+const {
+    app,
+    BrowserWindow,
+} = electron;
 let win;
 
 function createWindow() {
-  // Create the browser window.
+    // Create the browser window.
     win = new BrowserWindow({
         width: 1000,
         height: 800,
@@ -15,14 +18,14 @@ function createWindow() {
         icon: './icon.png',
     });
 
-  // and load the index.html of the app.
+    // and load the index.html of the app.
 
     win.loadURL('https://wx.qq.com');
 
-  // Open the DevTools.
-  // win.webContents.openDevTools()
+    // Open the DevTools.
+    // win.webContents.openDevTools()
 
-  // Emitted when the window is closed.
+    // Emitted when the window is closed.
     win.on('close', () => {
         try {
             app.exit(0);
@@ -31,9 +34,9 @@ function createWindow() {
         }
     });
     win.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
+       // Dereference the window object, usually you would store windows
+       // in an array if your app supports multi windows, this is the time
+       // when you should delete the corresponding element.
         win = null;
     });
 }
@@ -45,14 +48,14 @@ app.on('ready', createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
+    // On macOS it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
     app.quit();
 });
 
 app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
     if (win === null) {
         createWindow();
     }
